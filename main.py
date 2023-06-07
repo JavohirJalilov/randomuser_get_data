b5ccd8df8edd9d601d2fcbf44d407b3bced24d85
JavohirJalilov <javohirjaliloff@gmail.com>
2023-06-07 10:15:29 +0500

    get data and write data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from pprint import pprint
+import json
 
 def get_data():
     """
@@ -7,7 +9,31 @@ def get_data():
     Returns:
         list: list of user data
     """
-    pass
+    users = []
+
+    for i in range(10):
+        url = "https://randomuser.me/api/"
+        response = requests.get(url)
+        results = response.json()['results'][0]
+        name = results['name']
+        first_name = name['first']
+        last_name = name['last']
+        phone = results['phone']
+        city = results['location']['city']
+        email = results['email']
+
+        data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone": phone,
+            "city": city,
+            "email": email
+        }
+        users.append(data)
+
+        print(i)
+
+    return users
 
 def saveData(data, file_path):
     """
@@ -17,4 +43,9 @@ def saveData(data, file_path):
         data (list): list of user data
         file_path (str): path to file
     """
-    pass
+    f = open(file_path, mode='w')
+    data = json.dump(data, f, indent=4)
+    f.close()
+
+data = get_data()
+saveData(data, 'data.json')
